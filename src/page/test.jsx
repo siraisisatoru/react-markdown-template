effc1071a4860797aa5a65901293a598335b63f0
白石悟 <b_whk@yahoo.com.hk>
2024-02-12 12:52:43 +1100

    update test page

--- src/page/test.jsx
+++ src/page/test.jsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from "../utils/themeContext";
 import Nav_bar from "../utils/nav_bar";
-import MarkdownPage from "../utils/markdownPage";
+import MarkdownRender from "../utils/markdownRender";
 import mdstr from "./../Notes/markdownCheatsheet.md?raw";
-
 
 function TestPage() {
     return (
         <>
             <ThemeProvider>
                 <Nav_bar></Nav_bar>
-                <MarkdownPage mdstr={mdstr}></MarkdownPage>
+                <MarkdownRender mdstr={mdstr}></MarkdownRender>
             </ThemeProvider>
         </>
     );
